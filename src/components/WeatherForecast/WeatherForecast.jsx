1f1c76c8fb0d08b96051d18e52cdf548c24e16c1
Nick Choate <nickchoate@berkeley.edu>
2026-01-07 11:20:06 -0800

    done

---
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,14 @@
+import './WeatherForecast.css'
+
+const ForecastComponent = (props) => {
+  return (
+    <div className="weather">
+        <h2>{props.day}</h2>
+        <img src={props.img} alt={props.imgAlt} />
+        <p><span>conditions: </span>{props.conditions}</p>
+        <p><span>time: </span>{props.time}</p>
+    </div>
+  );
+};
+
+export default ForecastComponent;
